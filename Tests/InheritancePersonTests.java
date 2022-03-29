73b6680f5a5fee594b42a7f09a1857c2ccd7355b
FDIL <fdil.fadilh1@gmail.com>
2022-03-29 12:41:41 -0600

    Added test for AdultMale getters
    Added in test for AdultMale getters.
    Tests static getters for AdultMale returns correct value.

--- Tests/InheritancePersonTests.java
+++ Tests/InheritancePersonTests.java
@@ -10,12 +10,13 @@ public class InheritancePersonTests {
     
     @Before
     public void setUp() {
-        AdultMale adultMale = new AdultMale(10, 20, 30, 40, 10000);
+        AdultMale adultMale = new AdultMale(5, 25, 30, 40, 10000);
         AdultFemale adultFemale = new AdultFemale(10,20,30,40,10000);
+        ChildOverEight childOverEight = new ChildOverEight(15, 15, 35, 35, 10_000);
     }
-
+    
     @Test
-    public void AdultFemaleGetters() {
+    public void AdultFemaleGettersTest() {
         // Tests if getters return the correct value
         double expected = 1000;
         double actual = AdultFemale.getGrains();
@@ -36,5 +37,29 @@ public class InheritancePersonTests {
         expected = 10000;
         actual = AdultFemale.getCalories();
         assertEquals("Expected total calorie intake to be 10 000.", expected, actual, 0.0001);
+    }
+
+    @Test
+    public void AdultMaleGettersTest() {
+        // Tests if AdultMale getters return correct value
+        double expected = 10_000;
+        double actual = AdultMale.getCalories();
+        assertEquals("Expected total calorie intake to be 10 000.", expected, actual, 0.0001);
+
+        expected = 0.05*10_000;
+        actual = AdultMale.getGrains();
+        assertEquals("Expected grain intake to be 500.", expected, actual, 0.0001);
+
+        expected = 0.25*10_000;
+        actual = AdultMale.getVeggies();
+        assertEquals("Expected veggie intake to be 2500.", expected, actual, 0.0001);
+
+        expected = 0.3*10_000;
+        actual = AdultMale.getProtiens();
+        assertEquals("Expected veggie intake to be 3000.", expected, actual, 0.0001);
+
+        expected = 0.4*10_000;
+        actual = AdultMale.getOthers();
+        assertEquals("Expected veggie intake to be 4000.", expected, actual, 0.0001);
     }
 }
