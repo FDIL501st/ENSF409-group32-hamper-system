aa2d7855ee6bcda69eba68c117ef9ba2401df05e
FDIL <fdil.fadilh1@gmail.com>
2022-03-30 16:09:11 -0600

    Create FoodInventory.java
    Added in all its private methods. Made setters and getters.
    Added in stubs for rest of class methods.

---
+++ edu/ucalgary/ensf409/FoodInventory.java
@@ -0,0 +1,74 @@
+package edu.ucalgary.ensf409;
+
+import java.util.ArrayList;
+/**
+ * @since 1.0
+ * @version 1.0
+ */
+public class FoodInventory {
+    private ArrayList<String> inventory = new ArrayList<>();
+    private ArrayList<String> toBeRemoved = new ArrayList<>();
+    private int inventoryProteinCalories;
+    private int inventoryVeggieCalories;
+    private int inventoryGrainCalories;
+    private int inventoryOtherCalories;
+
+    public FoodInventory() {
+
+    }
+
+    public boolean readDatabaseInventory() {
+        return true;
+    }
+
+    public boolean updateDatabase() {
+        return true;
+    }
+
+    public boolean checkShortage() {
+        return true;
+    }
+
+    public boolean removeHamper(ArrayList<String> hamperContents) {
+        return true;
+    }
+
+    //Getters and Setters
+    public ArrayList<String> getInventory() { 
+        return inventory; 
+    }
+
+    public int getInventoryProteinCalories() {
+        return inventoryProteinCalories;
+    }
+
+    public void setInventoryProteinCalories(int inventoryProteinCalories) {
+        this.inventoryProteinCalories = inventoryProteinCalories;
+    }
+
+    public int getInventoryVeggieCalories() {
+        return inventoryVeggieCalories;
+    }
+
+    public void setInventoryVeggieCalories(int inventoryVeggieCalories) {
+        this.inventoryVeggieCalories = inventoryVeggieCalories;
+    }
+
+    public int getInventoryGrainCalories() {
+        return inventoryGrainCalories;
+    }
+
+    public void setInventoryGrainCalories(int inventoryGrainCalories) {
+        this.inventoryGrainCalories = inventoryGrainCalories;
+    }
+
+    public int getInventoryOtherCalories() {
+        return inventoryOtherCalories;
+    }
+
+    public void setInventoryOtherCalories(int inventoryOtherCalories) {
+        this.inventoryOtherCalories = inventoryOtherCalories;
+    }
+
+    
+}
