3406aa1cacf847588632a09c0076f8d95de7cc20
Saman Hosseini <101019594+pppix@users.noreply.github.com>
2022-03-30 20:26:39 -0600

    Add files via upload

---
+++ Tests/HamperRequestTest.java
@@ -0,0 +1,100 @@
+/**
+@author Saman Hosseini <a href="mailto:saman.hosseini@ucalgary.ca">saman.hosseini@ucalgary.ca</a>
+@version 1.1
+@since 1.0 
+*/
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.*;
+
+public class HamperRequestTest{
+	
+	final int ADULTMALES = 1;
+	final int ADULTFEMALES = 1;
+	final int CHILDU8 = 1;
+	final int CHILDO8 = 1;
+	
+	public void testGetAddHampers(){
+		
+		//// Setting up variables for testing.
+		
+		// Create a new hamper object
+		Hamper myHamper = new Hamper(ADULTMALES, ADULTFEMALES, CHILDU8, CHILDO8);
+		
+		// Create a new hamper arraylist with two hamper objects.
+		ArrayList<Hamper> myHamperList = new ArrayList<Hamper>();
+		myHamperList.add(myHamper);
+		myHamperList.add(myHamper);
+		
+		// Setup an array of one hamper.
+		Hamper[] hamperArr = { myHamper };
+		
+		// Create HamperRequest object for testing.
+		HamperRequest myRequest = new HamperRequest(hamperArr);
+		
+		// Add a new Hamper object with addHamper.
+		myRequest.addHamper(myHamper);
+		
+		//// Testing.
+		
+		// Use the getHampers() method to retrieve the actual hamper arraylist from the HamperRequest object.
+		ArrayList<Hamper> actualHamperList = myRequest.getHampers();
+		
+		assertEquals("Value of getHampers() did not match what was expected", myHamperList, actualHamperList);
+		
+	}
+	
+	public void testGetHampers(){
+		
+		//// Setting up variables for testing.
+		
+		// Create a new hamper object
+		Hamper myHamper = new Hamper(ADULTMALES, ADULTFEMALES, CHILDU8, CHILDO8);
+		
+		// Create a new hamper arraylist with one hamper object.
+		ArrayList<Hamper> myHamperList = new ArrayList<Hamper>();
+		myHamperList.add(myHamper);
+		
+		// Setup an array of one hamper.
+		Hamper[] hamperArr = { myHamper };
+		
+		// Create HamperRequest object for testing.
+		HamperRequest myRequest = new HamperRequest(hamperArr);
+		
+		//// Testing.
+		
+		// Use the getHampers() method to retrieve the actual hamper arraylist from the HamperRequest object.
+		ArrayList<Hamper> actualHamperList = myRequest.getHampers();
+		
+		assertEquals("Value of getHampers() did not match what was expected", myHamperList, actualHamperList);
+		
+	}
+	
+	public void testHamperRequestConstructor(){
+		
+		//// Setting up variables for testing.
+		
+		// Create a new hamper object
+		Hamper myHamper = new Hamper(ADULTMALES, ADULTFEMALES, CHILDU8, CHILDO8);
+		
+		// Setup an array of one hamper.
+		Hamper[] hamperArr = { myHamper };
+		
+		// Create HamperRequest object for testing.
+		HamperRequest myRequest = new HamperRequest(hamperArr);
+		
+		// Variables for status
+        boolean isNull = false;
+		
+		if(myRequest == null){
+			isNull = true;
+		}
+		
+		//// Testing.
+		
+		assertFalse("The HamperRequest object initialized to null.", isNull);
+		
+	}
+	
+}
