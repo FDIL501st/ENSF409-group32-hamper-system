737fddd09cc7af3351970c1d565add58eb113ec0
tanvirhaer <77709533+tanvirhaer@users.noreply.github.com>
2022-04-05 18:11:31 -0600

    Missing calculate methods for now

---
+++ edu/ucalgary/ensf409/FoodCalculator.java
@@ -0,0 +1,53 @@
+package edu.ucalgary.ensf409;
+
+public class FoodCalculator{
+    private double wholeGrainCalories;
+    private double fruitsVeggiesCalories;
+    private double proteinCalories;
+    private double otherCalories;
+    private double totalCalories;
+
+    private int numAdultMales;
+    private int numAdultFemales;
+    private int numChildrenU8;
+    private int numChildrenO8;
+
+    Arraylist<String[]> hamperFoodCombo = new ArrayList<String[]>();
+
+    // Constructor
+    public FoodCalculator(int numAdultMales, int numAdultFemales, int numChildrenU8, int numChildrenO8){
+        this.numAdultMales = numAdultMales;
+        this.numAdultFemales = numAdultFemales;
+        this.numChildrenU8 = numChildrenU8;
+        this.numChildrenO8 = numChildrenO8;
+    }
+
+    //Getters for numbers of people
+    public int getNumAdultMales() { return this.numAdultMales; }
+    public int getNumAdultFemales() { return this.numAdultFemales; }
+    public int getNumChildrenU8() { return this.numChildrenU8; }
+    public int getNumChildrenO8() { return this.numChildrenO8; }
+
+    //Getters for different types of calories
+    public double getWholeGrainCalories() { return this.wholeGrainCalories; }
+    public double getFruitsVeggiesCalories() { return this.fruitsVeggiesCalories; }
+    public double getProteinCalories() { return this.proteinCalories; }
+    public double getOtherCalories() { return this.otherCalories; }
+    public double getTotalCalories() { return this.totalCalories; }
+
+    //Getter for Hamper Food Combo
+    public ArrayList<String[]> getHamperFoodCombo() { return this.hamperFoodCombo; }
+
+    
+    // MISSING CALCULATE METHODS
+
+    public void calculateWholeGrainCalories(){}
+    public void calculateFruitsVeggiesCalories(){}
+    public void calculateProteinCalories(){}
+    public void calculateOtherCalories(){}
+    public void calculateTotalCalories(){}
+
+    public void calculateFoodCombos() {}
+
+
+}
