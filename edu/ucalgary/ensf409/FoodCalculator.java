d1535b3aab4088da4d12d553eb9b420883e89ce9
tanvirhaer <77709533+tanvirhaer@users.noreply.github.com>
2022-04-14 16:43:54 -0600

    added comments

--- edu/ucalgary/ensf409/FoodCalculator.java
+++ edu/ucalgary/ensf409/FoodCalculator.java
@@ -2,7 +2,10 @@
  * <h1>FoodCalculator</h1>
  * Project for ENSF 409
  * <p>
- * @author Group 32
+ * @author Adem
+ * @author Fadil
+ * @author Sam
+ * @author Tanvir Haer <a href="mailto:tanvir.haer@ucalgary.ca">tanvir.haer@ucalgary.ca</a> | UCID: 30039188
  * @version 1.6
  * @since 1.0
  */
@@ -24,7 +27,9 @@ public class FoodCalculator{
     private int numChildrenU8;
     private int numChildrenO8;
 
-    private ArrayList<String[]> hamperFoodCombo = new ArrayList<String[]>();
+    private ArrayList<String[]> hamperFoodCombo = new ArrayList<String[]>(); // optimum hamper combo storage variable
+
+
 
     private Comparator<ArrayList<String[]>> ascendingHamperCalories = new Comparator<ArrayList<String[]>>() {
         @Override
@@ -35,7 +40,10 @@ public class FoodCalculator{
         }
     };
 
-    // Constructor
+    /**
+     * Constructor to set the numbers of types of people 
+     * and calculate the numbers of calories needed for the hamper
+     */
     public FoodCalculator(int numAdultMales, int numAdultFemales, int numChildrenU8, int numChildrenO8){
         this.numAdultMales = numAdultMales;
         this.numAdultFemales = numAdultFemales;
@@ -49,25 +57,36 @@ public class FoodCalculator{
         calculateTotalCalories();
     }
 
-    //Getters for numbers of people
+    /**
+     * Getters for numbers of adult males, adult females, children over 8, and children under 8 in the hamper
+     */
     public int getNumAdultMales() { return this.numAdultMales; }
     public int getNumAdultFemales() { return this.numAdultFemales; }
     public int getNumChildrenU8() { return this.numChildrenU8; }
     public int getNumChildrenO8() { return this.numChildrenO8; }
 
-    //Getters for different types of calories
+    /**
+     * Getters for different types of calories needed for the hamper
+     */
     public double getWholeGrainCalories() { return this.wholeGrainCalories; }
     public double getFruitsVeggiesCalories() { return this.fruitsVeggiesCalories; }
     public double getProteinCalories() { return this.proteinCalories; }
     public double getOtherCalories() { return this.otherCalories; }
     public double getTotalCalories() { return this.totalCalories; }
 
-    //Getter for Hamper Food Combo
+    /**
+     * Getter for optimum Hamper Food Combo
+     */
     public ArrayList<String[]> getHamperFoodCombo() { return this.hamperFoodCombo; }
 
-    //Getter for comparator
+    /**
+     * Getter for comparator
+     */
     public Comparator<ArrayList<String[]>> getComparator() {return ascendingHamperCalories;}
-    //Calculate the total number of whole grain calories needed
+    
+    /**
+     * Calculates the total number of whole grain calories needed for the hamper per day
+     */
     public void calculateWholeGrainCalories(){
 
         double cals = numAdultMales * AdultMale.getGrains()
@@ -78,7 +97,9 @@ public class FoodCalculator{
         this.wholeGrainCalories = cals;
     }
 
-    //Calculate the total number of fruits/veggies calories needed
+    /**
+     * Calculates the total number of fruits/veggies calories needed for the hamper per day
+     */
     public void calculateFruitsVeggiesCalories(){
 
         double cals = numAdultMales * AdultMale.getVeggies()
@@ -89,7 +110,9 @@ public class FoodCalculator{
         this.fruitsVeggiesCalories = cals;
     }
 
-    //Calculate the total number of protein calories needed
+    /**
+     * Calculates the total number of protein calories needed for the hamper per day
+     */
     public void calculateProteinCalories(){
 
         double cals = numAdultMales*AdultMale.getProteins()
@@ -100,7 +123,9 @@ public class FoodCalculator{
         this.proteinCalories = cals;
     }
 
-    //Calculate the total number of other calories needed
+    /**
+     * Calculates the total number of other calories needed for the hamper per day
+     */
     public void calculateOtherCalories(){
         double cals = numAdultMales*AdultMale.getOthers()
         + numAdultFemales * AdultFemale.getOthers()
@@ -110,7 +135,9 @@ public class FoodCalculator{
         this.otherCalories = cals;
     }
 
-    //Calculate the total number of calories needed
+    /**
+     * Calculates the total number of calories needed for the hamper per day
+     */
     public void calculateTotalCalories(){
         double cals = numAdultMales * AdultMale.getCalories()
         + numAdultFemales * AdultFemale.getCalories()
