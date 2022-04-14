6507c92b78388914fdc392e6b9b8563a8a31c3d2
ademsoufi <77556297+ademsoufi@users.noreply.github.com>
2022-04-14 13:03:03 -0600

    Updated author documentation to make it consistent

--- edu/ucalgary/ensf409/ChildUnderEight.java
+++ edu/ucalgary/ensf409/ChildUnderEight.java
@@ -1,8 +1,14 @@
 package edu.ucalgary.ensf409;
+
 /**
- * @since 1.0
+ * <h1>ChildUnderEight</h1>
+ * Project for ENSF 409
+ * <p>
+ * @author Group 32
  * @version 1.3
+ * @since 1.0
  */
+
 public class ChildUnderEight extends Person{
     private static double grains;
     private static double veggies;
