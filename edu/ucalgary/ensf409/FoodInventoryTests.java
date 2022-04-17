d755c25d11d5420048828826efacb97bd372c242
FDIL <fdil.fadilh1@gmail.com>
2022-04-17 17:14:21 -0600

    Update FoodInventoryTests.java
    All of FoodInventory tests works.

--- edu/ucalgary/ensf409/FoodInventoryTests.java
+++ edu/ucalgary/ensf409/FoodInventoryTests.java
@@ -4,6 +4,8 @@ import org.junit.*;
 import static org.junit.Assert.*;
 import java.util.*;
 
+import javax.xml.crypto.Data;
+
 /* Tests for the FoodInventory Class */
 
 public class FoodInventoryTests{
@@ -23,37 +25,40 @@ public class FoodInventoryTests{
         expectedFoods.add(yam);
     
         inventory.setInventory(expectedFoods);
-        Arraylist<String[]> actualFoods = getInventory();
+        ArrayList<String[]> actualFoods = FoodInventory.getInventory();
         assertEquals("getInventory did not return the correct values", expectedFoods, actualFoods);
     }
     
     // Test that the readDataBaseInventory functions return true indicating successful read of database
     @Test
     public void readDatabaseInventoryTest(){
+        DatabaseReader.initializeConnection();
         FoodInventory inventory = new FoodInventory();
         boolean expected = true;
         boolean actual = inventory.readDatabaseInventory();
+        DatabaseReader.close();
         assertEquals("readDatabaseInventory did not return the correct boolean value", expected, actual);
     }
     
     // Test that updateDatabaseTest return true indicating successful update of databases
     @Test
     public void updateDatabaseTest(){
+        DatabaseReader.initializeConnection();
         FoodInventory inventory = new FoodInventory();
         boolean expected = true;
         boolean actual = inventory.updateDatabase();
+        DatabaseReader.close();
         assertEquals("updateDatabase did not return the correct boolean value", expected, actual);
     }
     
     // Test that checkShortageTest returns true when there is a shortage
     @Test
     public void checkShortageTest(){
-        FoodInventory inventory = new FoodInventory();
-        ArrayList<String> expectedFoods = new ArrayList<>();
-        inventory.setInventory();
-    
+        ArrayList<String[]> expectedFoods = new ArrayList<>();
+        FoodInventory.setInventory(expectedFoods);
+        double[] neededCalories = {10.1, 11, 23, 45, 56};
         boolean expected = true;
-        boolean actual = inventory.checkShortage();
+        boolean actual = FoodInventory.checkShortage(neededCalories);
     
         assertEquals("checkShortageTest did not return the correct boolean value", expected, actual);
     }
@@ -61,26 +66,26 @@ public class FoodInventoryTests{
     // Test the getters and setters of FoodInventory
     @Test
     public void setInventoryCaloriesTest(){
-        int expectedInventoryProteinCals = 23010;
-        int expectedInventoryVeggieCals = 9820;
-        int expectedInventoryGrainCals = 12000;
-        int expectedInventoryOtherCals = 34200;
+        double expectedInventoryProteinCals = 23010;
+        double expectedInventoryVeggieCals = 9820;
+        double expectedInventoryGrainCals = 12000;
+        double expectedInventoryOtherCals = 34200;
     
         FoodInventory inventory = new FoodInventory();
         inventory.setInventoryProteinCalories(expectedInventoryProteinCals);
         inventory.setInventoryVeggieCalories(expectedInventoryVeggieCals);
         inventory.setInventoryGrainCalories(expectedInventoryGrainCals);
-        inventory.setInventoryOtheralories(expectedInventoryOtherCals);
+        inventory.setInventoryOtherCalories(expectedInventoryOtherCals);
     
-        int actualInventoryProteinCals = inventory.getInventoryProteinCalories();
-        int actualInventoryVeggieCals = inventory.getInventoryVeggieCalories();
-        int actualInventoryGrainCals = inventory.getInventoryGrainalories();
-        int actualInventoryOtherCals = inventory.getInventoryOtherCalories();
+        double actualInventoryProteinCals = inventory.getInventoryProteinCalories();
+        double actualInventoryVeggieCals = inventory.getInventoryVeggieCalories();
+        double actualInventoryGrainCals = inventory.getInventoryGrainCalories();
+        double actualInventoryOtherCals = inventory.getInventoryOtherCalories();
     
-        assertEquals("Incorrect number of Protein Calories", expectedInventoryProteinCals, actualInventoryProteinCals);
-        assertEquals("Incorrect number of Veggie Calories", expectedInventoryVeggieCals, actualInventoryVeggieCals);
-        assertEquals("Incorrect number of Grain Calories", expectedInventoryGrainCals, actualInventoryGrainCals);
-        assertEquals("Incorrect number of Other Calories", expectedInventoryOtherCals, actualInventoryOtherCals);
+        assertEquals("Incorrect number of Protein Calories", expectedInventoryProteinCals, actualInventoryProteinCals, 0.001);
+        assertEquals("Incorrect number of Veggie Calories", expectedInventoryVeggieCals, actualInventoryVeggieCals, 0.001);
+        assertEquals("Incorrect number of Grain Calories", expectedInventoryGrainCals, actualInventoryGrainCals, 0.001);
+        assertEquals("Incorrect number of Other Calories", expectedInventoryOtherCals, actualInventoryOtherCals, 0.001);
     }
 
     @Test
